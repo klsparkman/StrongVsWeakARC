b7f4d7f95c5a711ed2f03b3ea56750864bbec0a5
Kelsey Sparkman <kelseysparkman14@gmail.com>
2020-03-24 22:05:48 -0600

    Finished demo project

--- StrongVsWeakARC/StrongVsWeakARC/ViewController.swift
+++ StrongVsWeakARC/StrongVsWeakARC/ViewController.swift
@@ -12,9 +12,47 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        var jayden: Person?
+        var unit4PP: Apartment?
+        
+        jayden = Person(name: "Jayden")
+        unit4PP = Apartment(unit: "4PP")
+        
+        jayden?.apartment = unit4PP
+        unit4PP?.tenant = jayden
+        
+//        jayden = nil
+//        unit4PP = nil
     }
-
 
 }
 
+class Person {
+    let name: String
+    var apartment: Apartment?
+    
+    init(name: String) {
+        self.name = name
+        print("Name \(name) is being initialized")
+    }
+    //Only so we can see it in the consol
+    deinit {
+        print("\(name) is is being deinitalized")
+    }
+}
+
+class Apartment {
+    
+    let unit: String
+    weak var tenant: Person?
+    
+    init(unit: String) {
+        self.unit = unit
+        print("\(unit) is being initialized.")
+    }
+    //Only so we can see it in the consol
+    deinit {
+           print("\(unit) is is being deinitalized")
+       }
+}
